89b26951564f067f212e80948d9cff6d0dd10174
Jose Alfredo Valladares <josevalladaresvillanueva@gmail.com>
2024-10-23 15:48:20 -0600

    Alfredo Local

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bienvenido al sitio
                 </div>
 
                 <div class="links">
